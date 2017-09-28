de97aac71f671a07ab46eb3f086ea79a17c4d40a
Brandon Villa <bornofos@gmail.com>
2017-09-28 10:36:26 -0500

    Finished

--- Cliente.java
+++ Cliente.java
@@ -15,36 +15,49 @@ class Cliente {
         String message;
         Socket skCliente;
         skCliente = new Socket( HOST , PUERTO );
-        do {
-          in = new BufferedReader(new InputStreamReader(skCliente.getInputStream()));
-          out = new PrintWriter(skCliente.getOutputStream(), true);
-          System.out.println(in.readLine());
-          System.out.println(in.readLine());
-          System.out.println(in.readLine());
+        in = new BufferedReader(new InputStreamReader(skCliente.getInputStream()));
+        out = new PrintWriter(skCliente.getOutputStream(), true);
+        
+        receive(in, skCliente);
+        send(out, sc);
 
-          Thread.sleep(200);
-          text = sc.nextLine();
-          // System.out.print("Texto: ");
-          // message = sc.nextLine();
-          out.println(text);
-          // out.println(message);
-          out.flush();
-          System.out.println(in.readLine());
-          System.out.println(in.readLine());
-
-          System.out.print("Texto: ");
-          String m2 = sc.nextLine();
-          out.println(m2);
-          
-          System.out.println(in.readLine());
-
-      
-        } while ( !text.equals("bye") );
-        skCliente.close(); 
+  
     } catch( Exception e ) { 
       System.out.println( e.getMessage() ); 
     } 
+  }
+
+  public static void send( PrintWriter out, Scanner sc ) throws Exception {
+    new Thread(new Runnable (){
+      public void run() {
+        System.out.println("send run()");
+        while(true) {
+          String text = sc.nextLine();
+          if (text.equals("BYE"))
+            break;
+          out.println(text);
+          out.flush();
+        }
+      }
+    }).start();
   } 
+
+  public static void receive( BufferedReader in, Socket skCliente ) throws Exception {
+    new Thread( new Runnable() {
+
+      public void run() {
+        try {
+          System.out.println("receive run()");
+          while(true)
+            System.out.println(in.readLine());
+        
+        } catch( Exception e) {
+          e.printStackTrace();
+        }
+                  
+      }
+    }).start();
+  }
   public static void main( String[] args ) { 
     new Cliente(); 
   }
